770ff918f990f88329263c8a9dc7d6cf9c145768
Mayur Pawar <62331235+programmerMayur@users.noreply.github.com>
2020-09-30 19:11:20 +0530

    Add informative comments

--- Code/Chapter1-2.py
+++ Code/Chapter1-2.py
@@ -1,13 +1,17 @@
 import tkinter as tk
 
+#initialize the class root
 class Root(tk.Tk):
     def __init__(self):
         super().__init__()
-
+        
+        #lable initialization done here
         self.label = tk.Label(self, text="Hello World", padx=5, pady=5)
-
+        
+        #this pack() method basically used for packs widgets in rows or columns 
         self.label.pack()
 
 if __name__ == "__main__":
+    #call the Root class
     root = Root()
     root.mainloop()
